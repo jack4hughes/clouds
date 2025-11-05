04f52aa73659499e43b68042c25d0693ce00f379
Jack Hughes <jack4.hughes@live.uwe.ac.uk>
2025-11-05 14:42:48 +0000

    inverted y axis of landmark_cloud.py

--- landmark_cloud.py
+++ landmark_cloud.py
@@ -72,4 +72,4 @@ def update_landmark_mean_dot(particle: Particle, landmark_mean_dot: LandmarkMean
     """Updates the landmarks position based off data contained within"""
     index = landmark_mean_dot.index
     position = particle.landmarks[index, :]
-    landmark_mean_dot.setPos(position[0], position[1])
+    landmark_mean_dot.setPos(position[0], -position[1])
